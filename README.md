ad416be557889512dbd13890025a0704a50b01be
Giggio13 <129890176+Giggio13@users.noreply.github.com>
2025-03-07 20:40:59 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone de um projeto do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Amanda, te amo. Se você me ama, dá uma risadinha...linda!</h1>
+function Home() {
+  return <h1>Amanda, te amo. Se você me ama, dá uma risadinha...linda!</h1>;
 }
 
 export default Home;
